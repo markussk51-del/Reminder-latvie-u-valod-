2a4e5319e7d01e41b787fb90c0648b95b067b6b5
markussk51-del <markussk51@gmail.com>
2026-02-11 12:16:14 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "reminders-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
